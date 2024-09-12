5dd8ce332971ed0b069aeda443c116c701277055
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-09-12 13:54:54 +0200

    added useEffects for succes, gameover and pulses

--- simonSays-Eneko/src/App.jsx
+++ simonSays-Eneko/src/App.jsx
@@ -56,6 +56,70 @@ function App() {
   const [success, setSuccess] = useState(0);
   const [isGameOn, setIsGameOn] = useState(false);
 
+  const randomNumber = () => {
+    setIsAllowedToPlay(false);
+    const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    setSequence([...sequence,randomNumber]);
+    setTurn(turn+1);
+  }
+  const handleClick = (index) => {
+    if (isAllowedToPlay) {
+      play({id: colors[index].sound})
+      colors[index].ref.current.style.opacity = (1);
+      colors[index].ref.current.style.scale = (0.9);
+      setTimeout(() => {
+        colors[index].ref.current.style.opacity = (0.5);
+        colors[index].ref.current.style.scale = (1);
+        setCurrentGame([...currentGame,index]);
+        setPulses(pulses + 1);
+      }, speed / 2);
+    }
+  }
+  useEffect(() => {
+    if (pulses > 0) {
+      if (Number(sequence[pulses-1]) === Number(currentGame[pulses-1])) {
+        setSuccess(success+1);
+      } else {
+        const index = sequence[pulses-1]
+        if (index) colors[index].red.current.style.opacity = 1;
+        play({id: 'error'})
+        setTimeout(() => {
+          if (index) colors[index].red.current.style.opacity = 0.5;
+          setIsGameOn(false); 
+        }, speed * 2)
+        setIsAllowedToPlay(false);             
+      }
+    }
+  }, [pulses])
+
+  useEffect(() => {
+    if (!isGameOn) {
+      setSequence([]);
+      setCurrentGame([]);
+      setIsAllowedToPlay(false);
+      setSpeed(speedGame);
+      setSuccess(0);
+      setPulses(0);
+      setTurn(0);
+    }
+  }, [isGameOn])
+
+  useEffect(() => {
+    if (success === sequence.length && success > 0) {
+      setSpeed(speed - sequence.length * 2);
+      setTimeout(() => {
+        setSuccess(0);
+        setPulses(0);
+        setCurrentGame([]);
+        randomNumber();
+      }, 500);
+    }
+  })
+  
+  const initGame = () => {
+    randomNumber();
+    setIsGameOn(true);
+  }
   return (
     <>
     {
@@ -91,7 +155,7 @@ function App() {
     }
     </>
   )
-  
+
 }
 
 export default App
