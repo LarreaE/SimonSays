6ca3e458aca8cf005dbed937ec66592e0572e88c
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-09-12 12:59:59 +0200

    Cleaned vite default code and added color references

--- simonSays-Eneko/src/App.jsx
+++ simonSays-Eneko/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react'
+import useSound from 'use-sound';
+import simon from './assets/sounds/sprite.mp3'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const blueRef   = useRef(null);
+  const redRef    = useRef(null);
+  const yellowRef = useRef(null);
+  const greenRef  = useRef(null);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>HELLO</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
